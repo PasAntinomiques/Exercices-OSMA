6f6eaa899f3bf95bbefe58dab5794cb5c28b2d5b
PasAntinomiques <35699701+PasAntinomiques@users.noreply.github.com>
2018-01-24 13:53:48 +0100

    xx

--- TP3/src/Main.java
+++ TP3/src/Main.java
@@ -1,8 +1,28 @@
+import jade.core.ProfileImpl;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
 
 public class Main {
 
 	public static void main(String[] args) {
+//		Création de l'environnement
+		jade.core.Runtime rt = jade.core.Runtime.instance();
+		//Création du main container nommé POSSAMAI (1 on paramètre le constructeur, 2 on construit)
+		ProfileImpl pMain = new ProfileImpl(null, 2000, "POSSAMAI");
+		AgentContainer mc = rt.createMainContainer(pMain);
 		
+//		Déclaration de l'agent rma qui permet de créer une interface graphique (ICP)
+		AgentController rma;
+		try {
+//			On créé l'agent dans le container main
+//			Ces deux lignes sont entourées par un try/catch car elles peuvent générer une erreur
+//			1- nom de l'agent, 2- chemin de l'agent, 3-paramètre pour cet agent (String[])
+			rma = mc.createNewAgent("rma", "jade.tools.rma.rma", null);
+			rma.start();
+		} catch (StaleProxyException e) {
+			e.printStackTrace();
+		}
 
 	}
 
